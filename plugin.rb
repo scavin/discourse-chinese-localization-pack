8bebe639f7ce5c475c9fda484454be0350951c6b
scavin <scavina@gmail.com>
2025-02-24 11:05:53 +0800

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -6,7 +6,8 @@
 
 enabled_site_setting :zh_l10n_enabled
 
-gem('omniauth-weibo-oauth2', '0.5.2') # https://github.com/beenhero/omniauth-weibo-oauth2
+# gem('omniauth-weibo-oauth2', '0.5.2') # https://github.com/beenhero/omniauth-weibo-oauth2
+gem 'omniauth-weibo-oauth2', git: 'https://github.com/scavin/omniauth-weibo-oauth2', branch: 'main'
 
 register_svg_icon 'fab-weibo'
 register_svg_icon 'fab-qq'
